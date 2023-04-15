3f5012d1a736e0ec97a4ae503603d82207781d5f
ppg0921 <79003285+ppg0921@users.noreply.github.com>
2023-04-15 14:31:41 +0800

    DeadEndTraversal and ShortRoute function added

--- maze.py
+++ maze.py
@@ -25,7 +25,7 @@ class Maze:
         # For example, when parsing raw_data, you may create several Node objects.
         # Then you can store these objects into self.nodes.
         # Finally, add to nd_dict by {key(index): value(corresponding node)}
-
+        self.cmds = ""
         self.raw_data = pandas.read_csv(filepath).values
         self.nodes = []
         # key: index, value: the correspond nodedf = pandas.read_csv(filepath)
@@ -33,8 +33,8 @@ class Maze:
         # self.nodes.append(Node(-1))
         df = pandas.read_csv(filepath)
         self.nodeNum, column_count = df.shape
-        self.shortPath = LifoQueue(self.nodeNum)
-        self.actionQueue = Queue(self.nodeNum)
+        self.shortPath = LifoQueue(self.nodeNum+3)
+        self.actionQueue = Queue(self.nodeNum+3)
         self.carDirection = caroridir 
         # print("nodeNum: ", self.nodeNum)
         # print("maze[1][4]: ", df[Direction_maze[1]][4])
@@ -100,7 +100,7 @@ class Maze:
                 # print(currIndex)
                 # print("here1")
             self.shortPath.put(currIndex)       # put start point
-            return 1
+            return deadIndex
 
         return -1
 
@@ -118,14 +118,16 @@ class Maze:
 
         while unprocessed.empty() is False:
             tmpNode = self.nd_dict[unprocessed.get()]
-            print(tmpNode.getIndex())
+            # print(tmpNode.getIndex())
             nowDis = distance[tmpNode.getIndex()]
             for adj in tmpNode.getSuccessors():
                 Succ = adj[0]
+
                 if distance[Succ] == 0:
                     unprocessed.put(Succ)
                     distance[Succ] = nowDis+1
                     previous[Succ] = tmpNode.getIndex()
+                    # print("previous[", Succ, "] = ", tmpNode.getIndex())
                     if Succ == nd_to.getIndex():
                         print("Finish node found... Node ", Succ)
                         Found = True
@@ -137,7 +139,8 @@ class Maze:
         currIndex = nd_to.getIndex()
         while self.shortPath.empty() is False:
             self.shortPath.get()
-        while currIndex != nd_from.getIndex():
+        # print("nd_from.getIndex()", nd_from.getIndex())
+        while int(currIndex) != int(nd_from.getIndex()):
             self.shortPath.put(currIndex)
             # print("currIndex: ", currIndex)
             currIndex = previous[int(currIndex)]
@@ -191,24 +194,29 @@ class Maze:
         
         return None
 
-    def getActions(self, nodes):
+    def getActions(self, nodes, First = False):
         # TODO : given a sequence of nodes, return the corresponding action sequence
         # Tips : iterate through the nodes and use getAction() in each iteration
         if nodes.empty() is False:
             nowNode = self.nd_dict[nodes.get()]
             nextNode = self.nd_dict[nodes.get()]
             # print("Car_dir:", self.carDirection)
-            car_new_action, self.carDirection = self.getAction(self.carDirection, nowNode, nextNode, first = True)
+            car_new_action, self.carDirection = self.getAction(self.carDirection, nowNode, nextNode, first = First)
             self.actionQueue.put(car_new_action)
             # print("Car_dir:", self.carDirection)
             # print("car_new_action: ", car_new_action)
             while nodes.empty() is False:
                 nowNode = nextNode
                 nextNode = self.nd_dict[nodes.get()]
+                # print("nextNode.Index: ", nextNode.getIndex())
+                # print("nowNode.Index: ", nowNode.getIndex())
                 car_new_action, self.carDirection = self.getAction(self.carDirection, nowNode, nextNode)
+                # print("here1")
                 self.actionQueue.put(car_new_action)
+                
                 # print("actionQueue.put ", car_new_action)
                 # print("Car_dir:", self.carDirection)
+            # print("here2")
         if nodes.empty() is True:
             self.actionQueue.put(Action.HALT)
             # print("actionQueue.put ", Action.HALT)
@@ -223,30 +231,120 @@ class Maze:
             cmds += cmd[action-1]
         print(cmds)
         return cmds
+    
+    def putCmdtoCmds(self):
+        while self.actionQueue.empty() is False:
+            tmpAction = self.actionQueue.get()
+            # print(tmpAction)
+            if tmpAction == Action.U_TURN:
+                self.cmds += "b"
+            elif tmpAction == Action.ADVANCE:
+                self.cmds += "f"
+            elif tmpAction == Action.TURN_LEFT:
+                self.cmds += "l"
+            elif tmpAction == Action.TURN_RIGHT:
+                self.cmds += "r"
+        return None
 
     def strategy(self, nd):
         return self.BFS(nd)
 
-    def strategy_2(self, nd_from, nd_to):
-        return self.BFS_2(nd_from, nd_to)
+    def ShortRoute(self, nd_from, nd_to):
+        self.BFS_2(nd_from, nd_to)
+        self.getActions(self.shortPath, First = True)
+        self.putCmdtoCmds()
+    
+    def DeadEndTraversal(self, nd_start, nd_end):
+        startIndex = nd_start.getIndex()
+        nd_start.DeadVisited()
+        nd_end.DeadVisited()
+        currNode = nd_start
+        dead = self.BFS(currNode)
+        while dead != -1:
+            self.getActions(self.shortPath, currNode.getIndex() == startIndex)
+            self.putCmdtoCmds()
+            # while(self.actionQueue.empty() is False):
+            #     tmpAction = self.actionQueue.get()
+            #     # print(tmpAction)
+            #     if tmpAction == Action.U_TURN:
+            #         print("b")
+            #     elif tmpAction == Action.ADVANCE:
+            #         print("f")
+            #     elif tmpAction == Action.TURN_LEFT:
+            #         print("l")
+            #     elif tmpAction == Action.TURN_RIGHT:
+            #         print("r")
+            print("end of BFS to dead end ", dead)
+            currNode = self.nd_dict[dead]
+            oldDead = dead
+            dead = self.BFS(currNode)
+        print("end of all BFS to deadends...")
+        self.putCmdtoCmds()
+    
+        self.BFS_2(self.nd_dict[oldDead], self.nd_dict[10])
+        self.getActions(self.shortPath, currNode.getIndex() == startIndex)
+        self.putCmdtoCmds()
+        # print("here")
+        #self.putCmdtoCmds()
+        # while(self.actionQueue.empty() is False):
+        #     tmpAction = self.actionQueue.get()
+        #     # print(tmpAction)
+        #     if tmpAction == Action.U_TURN:
+        #         print("b")
+        #     elif tmpAction == Action.ADVANCE:
+        #         print("f")
+        #     elif tmpAction == Action.TURN_LEFT:
+        #         print("l")
+        #     elif tmpAction == Action.TURN_RIGHT:
+        #         print("r")
+        
+        print("End of BFS_2...")
+
 
 
 if __name__ == '__main__':
-    testMaze = Maze('./data/maze_4_3-1.csv')
-    while()
-    testMaze.BFS_2(testMaze.nd_dict[1], testMaze.nd_dict[10])
-    testMaze.getActions(testMaze.shortPath)
-    while(testMaze.actionQueue.empty() is False):
-        tmpAction = testMaze.actionQueue.get()
-        # print(tmpAction)
-        if tmpAction == Action.U_TURN:
-            print("b")
-        elif tmpAction == Action.ADVANCE:
-            print("f")
-        elif tmpAction == Action.TURN_LEFT:
-            print("l")
-        elif tmpAction == Action.TURN_RIGHT:
-            print("r")
+    testMaze = Maze('./data/maze_4_3-2.csv')
+    # testMaze.DeadEndTraversal(testMaze.nd_dict[1], testMaze.nd_dict[10])
+    testMaze.ShortRoute(testMaze.nd_dict[1], testMaze.nd_dict[10])
+    print(testMaze.cmds)
+    
+    # startIndex = 1
+    # testMaze.nd_dict[startIndex].deadVisited = True;
+    # currNode = testMaze.nd_dict[startIndex]
+    # dead = testMaze.BFS(currNode)
+    # while dead != -1:
+    #     testMaze.getActions(testMaze.shortPath, currNode.getIndex() == startIndex)
+    #     while(testMaze.actionQueue.empty() is False):
+    #         tmpAction = testMaze.actionQueue.get()
+    #         # print(tmpAction)
+    #         if tmpAction == Action.U_TURN:
+    #             print("b")
+    #         elif tmpAction == Action.ADVANCE:
+    #             print("f")
+    #         elif tmpAction == Action.TURN_LEFT:
+    #             print("l")
+    #         elif tmpAction == Action.TURN_RIGHT:
+    #             print("r")
+    #     print("end of BFS to dead end ", dead)
+    #     currNode = testMaze.nd_dict[dead]
+    #     dead = testMaze.BFS(currNode)
+    # print("end of all BFS to deadends...")
+    
+        
+        
+    # testMaze.BFS_2(testMaze.nd_dict[dead], testMaze.nd_dict[10])
+    # testMaze.getActions(testMaze.shortPath)
+    # while(testMaze.actionQueue.empty() is False):
+    #     tmpAction = testMaze.actionQueue.get()
+    #     # print(tmpAction)
+    #     if tmpAction == Action.U_TURN:
+    #         print("b")
+    #     elif tmpAction == Action.ADVANCE:
+    #         print("f")
+    #     elif tmpAction == Action.TURN_LEFT:
+    #         print("l")
+    #     elif tmpAction == Action.TURN_RIGHT:
+    #         print("r")
         
         
     
